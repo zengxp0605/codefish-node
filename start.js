fe81fd1090cd0712f71742815fe95f570d253dd8
zengxp@home-win <xiaoping_zeng@foxmail.com>
2016-03-05 11:32:19 +0800

    add socket.io

--- start.js
+++ start.js
@@ -1,4 +1,25 @@
-var app = require("./app");
-var port = process.env.PORT || 5000;
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 5001;
 
-app.listen(port)
+var server = require('http').Server(app);
+
+  // app.set('views', __dirname + '/views');
+  // app.engine('html', require('ejs').renderFile);
+  // app.get('/', function(req, res) {
+  //   res.render('index.html');
+  // });
+  app.use('/',express.static(__dirname + '/views')); //指定静态资源目录
+
+  var io = require('socket.io').listen(server);
+  io.on('connection',function(socket){
+  	console.log('io connection [ok]');
+  	socket.on('login',function(data){
+  		console.log('server login: ',data);
+  	});
+  });
+
+//app.listen(port);
+server.listen(port,function(){
+	console.log('server listen *',port);
+});
